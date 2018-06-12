907e0f1541aa23805016ee8a178b6035ad6bf2a0
Alron <alronjan@yahoo.com>
2018-06-12 18:30:52 +0800

    Add constant page header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import 'semantic-ui-css/semantic.min.css';
 import React, {Component} from 'react';
 import './App.css';
+import {Container, Header, Segment} from 'semantic-ui-react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import LessonPage from "./pages/LessonPage"
 import MainPage from "./pages/MainPage";
@@ -9,14 +10,22 @@ export default class App extends Component {
 
     render() {
         return (
-  
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path='/' component={MainPage}/>
-                    <Route path='/:slug' component={LessonPage}/>
-                </Switch>
-            </BrowserRouter>
-
+            <Container text>
+                <Container fluid textAlign="center">
+                    <Segment inverted>
+                        <Header as='h2' inverted>
+                            Life on Life <br/>
+                            Devotional Group Material
+                        </Header>
+                    </Segment>
+                </Container>
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path='/' component={MainPage}/>
+                        <Route path='/:slug' component={LessonPage}/>
+                    </Switch>
+                </BrowserRouter>
+            </Container>
         )
     }
 }
