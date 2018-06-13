4fb77fc891e608ddc6e98de0f7a27861902cfb36
Alron <alronjan@yahoo.com>
2018-06-13 23:27:17 +0800

    Fix formatting for Lesson 2 group

--- src/lessons/Lesson_2_1.js
+++ src/lessons/Lesson_2_1.js
@@ -1,26 +1,28 @@
-export const lesson_2_1_content = "LEAD ON<br/>" +
-    "Question:<br/>" +
+export const lesson_2_1_content = "<h3>LEAD ON</h3>" +
+    "Question: " +
     "Share a particular mistake you did in the past which damaged a good relationship. <br/>" +
     "<br/>" +
-    "The Original Sin: Where it all started<br/>" +
+    "<b><i>The Original Sin: Where it all started</i></b><br/>" +
     "Shortly after the story of the world began, man falls into sin. The Serpent (who we later learn to be the Devil in the flesh -- see Revelation 12:9) came into the scene, deceiving the Woman (and Adam who was right there with her into disobeying a direct command from God (Genesis 3:6): the command not to eat of the fruit of the knowledge of good and evil. From then on, sin reigned over man and all creation. <br/>" +
     "<br/>" +
-    "OPEN WORD<br/>" +
-    "Genesis 3:16-19<br/>" +
+    "<h3>OPEN WORD</h3>" +
+    "<b><i>Genesis 3:16-19</i></b><br/>" +
     "16To the woman he said,“I will surely multiply your pain in childbearing; in pain you shall bring forth children. Your desire shall be contrary to[a] your husband, but he shall rule over you.” 17And to Adam he said “Because you have listened to the voice of your wife and have eaten of the tree of which I commanded you,‘You shall not eat of it,’ cursed is the ground because of you; in pain you shall eat of it all the days of your life, 18thorns and thistles it shall bring forth for you; and you shall eat the plants of the field.19By the sweat of your face you shall eat bread, till you return to the ground, for out of it you were taken; for you are dust, and to dust you shall return.”<br/>" +
     "<br/>" +
-    "Genesis 2:17<br/>" +
+    "<b><i>Genesis 2:17</i></b><br/>" +
     "17but of the tree of the knowledge of good and evil you shall not eat, for in the day that you eat of it you shall surely die.”<br/>" +
     "<br/>" +
     "As consequence, God curses the man, the woman and the serpent: painful work for man (3:17-19), pain in childbirth for the woman (3:16), and relationship struggles between each other (3:16) and death for both (2:17, 3:19). But sin’s consequences came with way more damage than it seems. Now, we’ll be looking at how sin has damaged man’s relationship with God.<br/>" +
     "<br/>" +
-    "Sin damaged man’s relationship with God and brought death <br/>" +
+    "<b><i>Sin damaged man’s relationship with God and brought death</i></b><br/>" +
     "After Adam and Eve’s sin, we then see them scrambling for ways to hide themselves from God’s presence (see Genesis 3:7-8, 10). As soon as they heard God walking in the garden, the two started displaying behaviour that proved their relationship with Him was not the way it used to be. Immediately, there was a gap in between man and God – a gap of fear, guilt and shame. This was because deep inside, they know that disobedience is fundamentally a distrust in God’s goodness and love. We know this ourselves, and feel this too in our own experience.<br/>" +
     "<br/>" +
     "They did not trust God’s word, choosing rather to gain knowledge of good and evil the wrong way.They disobeyed God, transgressing God’s command of not eating the forbidden fruit rather than enjoying the other fruits and vegetation which God has made available. By doing so, they have started to side against God, entertaining thoughts of rebellion against the One who created them and everything else for their enjoyment. When God came to confront them, they felt so afraid and ashamed that all they could do was hide their guilty faces. Same happens with us: the guilt from sin separates us from God’s holy presence.<br/>" +
     "<br/>" +
     "Nowadays, we consider death as “normal.” We are all bound to die from the day we were born.  That’s because death came with the sin and its curse (Genesis 2:17; also see Romans 6:23). Although God is merciful enough to not let them die immediately, as everyone might have expected, Adam and Eve, cut off from the Source and Author of life, has earned nothing but death and destruction because of their sin.<br/>" +
     "<br/>" +
-    "LIVE OUT<br/>" +
-    "Evaluate yourself and ask God in prayer to help you determine things in your life that are going against God’s will. <br/>" +
-    "Be accountable to the Lord by making a habit of confessing your sins to Him and to a godly and trustworthy person or group. <br/>"
+    "<h3>LIVE OUT</h3>" +
+    "<ul>" +
+    "<li>Evaluate yourself and ask God in prayer to help you determine things in your life that are going against God’s will.</li>" +
+    "<li>Be accountable to the Lord by making a habit of confessing your sins to Him and to a godly and trustworthy person or group.</li>" +
+    "</ul>"
--- src/lessons/Lesson_2_3.js
+++ src/lessons/Lesson_2_3.js
@@ -1,25 +1,24 @@
-export const lesson_2_3_content = "LEAD ON:<br/>" +
-    "Question:<br/>" +
+export const lesson_2_3_content = "<h3>LEAD ON</h3>" +
+    "Question: " +
     "Share a travel experience you had where you were able to see the wonders of God’s creation. <br/>" +
     "<br/>" +
-    "OPEN WORD:<br/>" +
+    "<h3>OPEN WORD</h3>" +
     "As God’s representatives, bearer’s of His image and stewards of creation on His behalf, man and woman were not the only ones affected by the Fall. Even the rest of creation suffers the consequences of sin. Originally, God called His creation “very good,” but as sin came God Himself cursed it (“the ground”) so that it brings suffering and pain to all its inhabitants. We can then say that we currently live in a fallen, broken world.<br/>" +
     "<br/>" +
-    "Creation was subjected to futility<br/>" +
-    "Paul, in Romans 8:20-22, tells us the “story behind the story,” what happened to God’s creation because of sin, and what it meant.<br/>" +
+    "<b><i>Creation was subjected to futility</i></b><br/>" +
+    "Paul, in Romans 8:20-22, tells us the “story behind the story,” what happened to God’s creation because of sin, and what it meant. " +
+    "20 For the creation was subjected to futility, not willingly, but because of him who subjected it, in hope 21that the creation itself will be set free from its bondage to corruption and obtain the freedom of the glory of the children of God. 22For we know that the whole creation has been groaning together in the pains of childbirth until now. (Romans 8:20–22)<br/>" +
     "<br/>" +
-    "20For the creation was subjected to futility, not willingly, but because of him who subjected it, in hope 21that the creation itself will be set free from its bondage to corruption and obtain the freedom of the glory of the children of God. 22For we know that the whole creation has been groaning together in the pains of childbirth until now. (Romans 8:20–22)<br/>" +
-    "<br/>" +
-    "Sin damages man’s relationship with the rest of God’s creation<br/>" +
+    "<b><i>Sin damages man’s relationship with the rest of God’s creation</i></b><br/>" +
     "Creation, or nature, as it is often called, started to malfunction.Though God created the world in glory and order, sin came in and brought disruption, death and decay. To this day, sin’s curse is still in effect, keeping God’s creation bound to corruption (see Romans 8:20), and so we suffer its continuous consequences in different ways, such as natural calamities and disasters like earthquakes, typhoons and tornadoes, floods, and the like. The earth used to be perfectly habitable for humans and animals; now, it has become a challenge for both to even survive it.<br/>" +
     "<br/>" +
-    "Work became difficult and painful<br/>" +
+    "<b><i>Work became difficult and painful</i></b><br/>" +
     "Originally, the ground, including all the trees and plant life that grow on it, would be Adam’s source of food, and thus source of life. However, as part of the curse, the ground and all its produce will make it significantly more difficult for humans’ life on earth. Fruits, crops and vegetation would still grow for man, but not after much difficulty and toil. Pain now will accompany man’s efforts to feed himself and sustain his and his family’s life. Man works hard and feeds to restore his strength, in order to continue the labor the next day; the cycle goes on until death. Work itself, though, is not a result nor a part of the curse, but painful work is.<br/>" +
     "<br/>" +
-    "Awaiting redemption...<br/>" +
+    "<b><i>Awaiting redemption...</i></b><br/>" +
     "Going back to Romans 8, we see this logic: As man, God’s designated head of creation, falls, so goes the rest of creation. Conversely, the only way creation will be set free from the bondage of the curse is that man be set free first. <br/>" +
     "<br/>" +
     "Creation itself knows this, and is groaning, says Paul. With each crushing tsunami, each movement of the tectonic plates underneath the ground, each boom of the thunderstorm, creation is wailing and bellyaching, as if in desperate anticipation of a saving act that will finally free it from bondage.<br/>" +
     "<br/>" +
-    "LIVE OUT:<br/>" +
+    "<h3>LIVE OUT</h3>" +
     "Share particular struggle/s in your life (personal, work, at home, etc.) and how you can be prayed for.<br/>"
