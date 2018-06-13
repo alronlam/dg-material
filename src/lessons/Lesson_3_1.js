71d6930fab7d0886e9a548c6102fa54707098a0b
Alron <alronjan@yahoo.com>
2018-06-13 23:33:16 +0800

    Fix formatting for Lesson 3 group

--- src/lessons/Lesson_3_1.js
+++ src/lessons/Lesson_3_1.js
@@ -1,18 +1,18 @@
-export const lesson_3_1_content = "LEAD ON<br/>" +
-    "Words of Wisdom<br/>" +
+export const lesson_3_1_content = "<h3>LEAD ON</h3>" +
+    "<b><i>Words of Wisdom</i></b><br/>" +
     "foreshadowing. To represent, indicate or typify beforehand (Merriam-Webster)<br/>" +
     "In many ways, Jesus is “foreshadowed” as the fulfillment of many promises and anticipations in the Old Testament .<br/>" +
     "<br/>" +
-    "OPEN WORD<br/>" +
-    "The Redemption Story<br/>" +
+    "<h3>OPEN WORD</h3>" +
+    "<b><i>The Redemption Story</i></b><br/>" +
     "Soon after man’s fall to sin, God begins his work of redemption. Though God delivered judgement, it was not as grave as everyone would have expected. Instead, he dealt graciously, showing acts that display his kindness and patience. In the same chapter (Genesis 3) where the Fall of all creation happens, we also find God start making headway towards regaining what was lost. This part of the story is called Redemption.<br/>" +
     "<br/>" +
-    "Curse of sin<br/>" +
+    "<b><i>Curse of sin</i></b><br/>" +
     "In verses 14 to 19, we see God’s curse (or curses) in response to the Original Sin, and He first speaks to the Serpent. In the moment, God does two things: pronounce a curse on the Serpent (verse 14) and announce His plan to finally defeat this crafty, deathly liar (verse 15). How? According to God’s word, the Woman’s “offspring” -- a Son -- is destined to kill the lying snake. <br/>" +
     "<br/>" +
     "So who is this Son? Is it Cain? Or is it Abel? If there is any killing involving the two sons, it’s between themselves -- Cain murdering his brother Abel! But not the snake. <br/>" +
     "<br/>" +
-    "Child of promise<br/>" +
+    "<b><i>Child of promise</i></b><br/>" +
     "We know three of Adam and Eve’s sons: Cain, Abel and Seth. Of course it can’t be Abel. It is also certainly not Cain, since we know him to resemble the serpent’s offspring more than he resembles Eve’s Savior-Son. We also know of Seth, one who is singled out in Genesis 4:25. Still, it’s not Seth. Read further, and you discover that though Adam and Eve had other sons and daughters, still you will not find the story of a Son of Eve crushing the snake’s head. <br/>" +
     "<br/>" +
     "Now, what we must realize is that when God said “offspring,” He is not exactly referring to someone who is literally a direct offspring of Eve, but a descendant coming out of a much later generation (although coming from the lineage of Seth). <br/>" +
@@ -28,9 +28,11 @@ export const lesson_3_1_content = "LEAD ON<br/>" +
     "Read Galatians 4:4:<br/>" +
     "[4] But when the fullness of time had come, God sent forth his Son, born of woman, born under the law,<br/>" +
     "<br/>" +
-    "Christ the Savior<br/>" +
+    "<b><i>Christ the Savior</i></b><br/>" +
     "Jesus the Christ is Eve’s Promised Son. He is the destined destroyer of the Ancient Serpent, who is Satan. He came to destroy the works of the devil, and to accomplish so much more in the name of Redemption. Notice also that the Woman was only given the name Eve after this promise was given, for the name means “mother of all living,” because from this point onward, she will be the mother of this anticipated Lifegiver. From this point on, history -- the story of God -- awaits the coming of Jesus Christ.<br/>" +
     "<br/>" +
-    "LIVE OUT <br/>" +
-    "Look back. Always be assured of the finished work of Christ in the past on your behalf. It’s easy to drift away into thinking that God accepts you on the basis of your own good works/deeds. Remember that it is Christ alone that makes you worthy of God’s acceptance and love.<br/>" +
-    "Shadow forth. If the Old Testament foreshadows Jesus Christ, let your life also shadow forth or reflect who He is and what He has done. Give ways on how your life demonstrates Christ-likeness."
+    "<h3>LIVE OUT</h3>" +
+    "<ul>" +
+    "<li>Look back. Always be assured of the finished work of Christ in the past on your behalf. It’s easy to drift away into thinking that God accepts you on the basis of your own good works/deeds. Remember that it is Christ alone that makes you worthy of God’s acceptance and love.</li>" +
+    "<li>Shadow forth. If the Old Testament foreshadows Jesus Christ, let your life also shadow forth or reflect who He is and what He has done. Give ways on how your life demonstrates Christ-likeness.</li>" +
+    "</ul>"
