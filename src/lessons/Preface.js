538ba89f400c59dfc0972720e491d2ceee70daab
Alron <alronjan@yahoo.com>
2018-08-18 17:05:52 +0800

    Add discipleship roadmap image

--- src/lessons/Preface.js
+++ src/lessons/Preface.js
@@ -1,4 +1,12 @@
-export const preface_content = "In Jesus we find Life. Christian life is, therefore, wholly dependent and centered around Jesus Christ and all He is for us – His Gospel and finished work being as of first importance." +
+export const preface_content = ""+
+
+"<center>" +
+"<img class='ui  image' src='./dg_roadmap.jpg'>" +
+"</center>" +
+"<br/><br/>"+
+
+
+"In Jesus we find Life. Christian life is, therefore, wholly dependent and centered around Jesus Christ and all He is for us – His Gospel and finished work being as of first importance." +
 "<br/><br/>" +
 "This life that we receive in Christ flows from Him, through us and to every aspect of our lives. We, Christians, will do well only if we make a habit of cultivating this life. This is why we have this as our aim: to live the Gospel-centered Christian life, and live it abundantly, by the power of His Spirit. Thus, we have planned and traced the paths for our discipleship journey with this aim in mind. This discipleship journey is one that ends only in eternity, when we finally get to see and be with our Lord and Savior." +
 "<br/><br/>" +
