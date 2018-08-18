8de9e94be9ac5492f015486250f7fe26931f213a
Alron <alronjan@yahoo.com>
2018-08-18 15:15:49 +0800

    Update Creation 1

--- src/lessons/Lesson_1_1.js
+++ src/lessons/Lesson_1_1.js
@@ -1,35 +1,53 @@
-export const lesson_1_1_content = "<h3>LEAD ON</h3>" +
-    "Question: " +
-    "What is your favourite book? Give a summary of what the book is all about.<br/>" +
-    "<br/>" +
-    "The meaning of something lies in its author. If you’re having trouble understanding what a particular phrase or part of a book means, the best thing to do is to ask the one who wrote it. If you want to know how to operate any kind of machine, such as a computer or a washing machine, you read through the manufacturer’s manual. <br/>" +
-    "<br/>" +
-    "In the same way, if you want to understand the meaning of life – of yours and of the whole universe – you’re going to have to go to its Author.<br/>" +
-    "<br/>" +
-    "<h3>OPEN WORD</h3>" +
-    "<b><i>Acts 3:15</i></b><br/>"+
-    "“…[Y]ou killed the Author of life, whom God raised from the dead. To this we are witnesses.” <br/>" +
-    "<br/>" +
-    "In a style that reminds us of the prophets of the Old Testament, Peter preaches to Jews both with zeal and poetic language, stating truth in beautiful paradox: This Jesus, a man condemned to die a criminal’s death on a cross, is actually the Author of all of life, the One from whom all things come and are for.<br/>" +
-    "<br/>" +
-    "Notice this irony of ironies? In effect he is saying, That guy you unjustly had killed? He’s actually the Creator of life. " +
-    "Indeed, He is the Author of life because He is not just human, but also divine. Read: <br/>" +
-    "<br/>" +
-    "<b><i>Genesis 1:1</i></b><br/>" +
-    "“In the beginning God created the heavens and the earth”<br/>" +
-    "<br/>" +
-    "<b><i>John 1:1-3</i></b><br/>" +
-    "“In the beginning was the Word, and the Word was with God, and the Word was God. 2 He was in the beginning with God. 3 All things were made through him, and without him was not anything made that was made.”<br/>" +
-    "<br/>" +
-    "<b><i>John 1:14</i></b><br/>" +
-    "“And the Word became flesh and dwelt among us, and we have seen his glory, glory as the only Son from the Father, full of grace and truth”<br/>" +
-    "<br/>" +
-    "He was the eternal Word (Greek: Logos; in John 1:1-14) who was with God and who was indeed God (John 1:1-2), and the One through which everything that exists was made (John 1:3). As the story goes, this Word took on flesh, and dwelled among men to live  – and even die – as a man. But, to further display His divine power, He then resurrected back to life after three days! Since Jesus is God, then He is and has been there since there was yet nothing – nothing but the Triune God – until this God decided to create (Genesis 1:1). <br/>" +
-    "<br/>" +
-    "So, all of God’s creation was created according to His assigned meaning and purpose, sealed with His imprint and design. If Jesus is the author then it is His intended purposes and plans for all things, including the lives of individuals like you and me, which must be the ultimate standard. We do not, then, get to decide which is true for us, or decide what life means for ourselves. If we want to understand life and how to live it the right way, we go nowhere else but to God through His Son, the Creator and Author of life.<br/>" +
-    "<br/>" +
-    "<h3>LIVE OUT</h3>" +
-    "<ul>"+
-    "<li>Make a habit of reading the Bible, regarding it as the highest authority over your life and your beliefs</li>" +
-    "<li>Prayerfully prepare for each lesson, asking God to grant you insight into and openness to His promptings.</li>" +
-    "</ul>"
+export const lesson_1_1_content = ""+
+"<h3>LEAD ON</h3>" +
+"Question: " +
+"What is your favourite book? Give a summary of what the book is all about.<br/>" +
+"<br/>" +
+
+"The meaning of something lies in its author. If you’re having trouble understanding what a particular phrase or part of a book means, the best thing to do is to ask the one who wrote it. If you want to know how to operate any kind of machine, such as a computer or a washing machine, you read through the manufacturer’s manual. <br/>" +
+"<br/>" +
+
+"In the same way, if you want to understand the meaning of life – of yours and of the whole universe – you’re going to have to go to its Author.<br/>" +
+"<br/>" +
+
+"<h3>OPEN WORD</h3>" +
+"<b><i>Acts 3:15</i></b><br/>"+
+"“…[Y]ou killed the Author of life, whom God raised from the dead. To this we are witnesses.” <br/>" +
+"<br/>" +
+
+"The book of Acts tells of the earliest history of the church, the very first events following Jesus Christ’s death, rising back to life and ascension to Heaven. At this particular time, Peter and John went to the temple of the Jews, and had just displayed a miraculous healing of a lame beggar. At the sight of the healed beggar, “walking and leaping and praising God,” the Jews (the people who got Jesus crucified) stood in utter amazement. And Peter took the opportunity to preach." +
+"<br/><br/>" +
+
+"In a style that reminds us of the prophets of the Old Testament, Peter preaches to Jews both with zeal and poetic language, stating truth in beautiful paradox: This Jesus, a man condemned to die a criminal’s death on a cross, is actually the Author of all of life, the One from whom all things come and are for.<br/>" +
+"<br/>" +
+
+"Notice this irony of ironies? In effect he is saying, “That guy you unjustly had killed? The man whose life you had taken? He’s actually the Creator of life.“ " +
+"Indeed, He is the Author of life because He is not just human, but also divine. Read: <br/>" +
+"<br/>" +
+
+"<b><i>Genesis 1:1</i></b><br/>" +
+"“In the beginning God created the heavens and the earth”<br/>" +
+"<br/>" +
+
+"<b><i>John 1:1-3</i></b><br/>" +
+"“In the beginning was the Word, and the Word was with God, and the Word was God. 2 He was in the beginning with God. 3 All things were made through him, and without him was not anything made that was made.”<br/>" +
+"<br/>" +
+
+"<b><i>John 1:14</i></b><br/>" +
+"“And the Word became flesh and dwelt among us, and we have seen his glory, glory as the only Son from the Father, full of grace and truth”<br/>" +
+"<br/>" +
+
+"He was the eternal Word (in John 1:1-14) who was with God and who was indeed God (John 1:1-2), and the One through which everything that exists was made (John 1:3). As the story goes, this Word took on flesh, and dwelled among men to live  — and even die — as a man. But, to further display His divine power, He then resurrected back to life after three days! <br/>" +
+"<br/>" +
+
+"Since Jesus is God, then He is and has been there since there was yet nothing – nothing but the Triune God – until this God decided to create (Genesis 1:1)." +
+"<br/><br/>" +
+
+"So, all of God’s creation was created according to His assigned meaning and purpose, sealed with His imprint and design. If Jesus is the Author then it is His intended purposes and plans for all things, including the lives of individuals like you and me, which must be the ultimate standard. We do not, then, get to decide which is true for us, or decide what life means for ourselves. If we want to understand life and how to live it the right way, we go nowhere else but to God through His Son, the Creator and Author of life.<br/>" +
+"<br/>" +
+
+"<h3>LIVE OUT</h3>" +
+"<ul>"+
+"<li>Make a habit of reading the Bible, regarding it as the highest authority over your life and your beliefs</li>" +
+"<li>Prayerfully prepare for each lesson, asking God to grant you insight into and openness to His promptings.</li>" +
+"</ul>"
