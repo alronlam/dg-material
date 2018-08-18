4a8767021f7abf7e724c0acfb9b8bf0ec3a72b36
Alron <alronjan@yahoo.com>
2018-08-18 16:33:14 +0800

    Update Intro Lesson

--- src/lessons/Lesson_Intro_Story_Of_Life.js
+++ src/lessons/Lesson_Intro_Story_Of_Life.js
@@ -1,35 +1,50 @@
 export const lesson_intro_story_of_life_content =
-    "<h3>What is the story of life?</h3>"+
-    "Your whole life depends on how you answer this question. You live your life according to the story you believe you are a part of. Like these, for example:<br/>" +
-    "<br/>" +
-    "If you believe the story of life is a story of chance, then you live believing that you are just a product of accidents, and that everything you do ultimately doesn’t really mean anything.<br/>" +
-    "<br/>" +
-    "If you believe the story of life is a story of struggle for power, then you will see everyone else as only either enemies to overpower or obstacles to overcome.<br/>" +
-    "<br/>" +
-    "If you believe the story of life is a story of enjoying everything in life while you can, then you spend life chasing all the pleasures you can get your hands on in this temporary world.<br/>" +
-    "<br/>" +
-    "<h3>What is life according to God?</h3>" +
-    "According to the Bible, this is God’s story....<br/>" +
-    "<br/>" +
-    "<b><i>God Created</i></b>" +
-    "<br/>" +
-    "Everything starts with God (Genesis 1:1). In fact, God is the center of all things. Everything moves and exists around Him and His good, pleasing and perfect will (Romans 11:36, 12:2b). Great. Beautiful. Infinitely glorious. This is God!" +
-    "Then, this great, great God began to create. This is the beginning of the universe — the beginning of life, and the birth of humanity.<br/>" +
-    "<br/>" +
-    "<b><i>Man Sinned</i></b>" +
-    "<br/>" +
-    "However, humanity soon disobeyed their Creator. This is called sin . From then on, all of humanity became sinful. All became sinners (no exception!), separated from God (Romans 3:23) and condemned to experience His righteous wrath — eternal punishment in hell. This is eternal death (Romans 6:23a).<br/>" +
-    "<br/>" +
-    "<b><i>God Redeemed</i></b>" +
-    "<br/>" +
-    "But God, being rich in mercy, out of His great love (Ephesians 2:4), sent a Savior — Jesus Christ — who came to: " +
-    "Live a perfect life (1 Peter 2:22), die a sacrificial death (1 Peter 2:24; Romans 5:8), and rise again to life (John 2:22), for the sake of those who will believe in him as Savior and Lord (John 3:16) — this is salvation. Those who believe have been forgiven forever (Ephesians 1:7), and are given the right to become blessed children of God (John 1:12), and have received life, life that continues beyond death… indeed, eternal life in God’s presence(John 5:24, 1 John 3:14), fully and forever (Psalm 16:11)!<br/>" +
-    "<br/>" +
-    "<h3>How, then, should life be lived?</h3>" +
-    "Life, then, must be lived by faith in Jesus Christ (Romans 1:17). Faith or believing in Jesus means trusting in Him alone for your salvation (1 Timothy 2:5), acknowledging and turning away from your sins (Acts 3:19), making a decision to give your whole life to Him as your Lord (Romans 10:9), and delighting in and treasuring Him as the source of perfect joy (Matthew 13:44; Psalm 16:11). <br/>" +
-    "<br/>" +
-    "This salvation is only received not by our works or any merits coming from us, but only by God’s amazing grace, purely undeserved blessing, freely given to His people (Ephesians 2:8-9; Romans 6:23).<br/>" +
-    "<br/>" +
-    "Do you believe? If yes, then it means you have been born again into God’s Kingdom (John 3:3-6)! Newness of life has come (Romans 6:4; 2 Corinthians 5:17), because the Lord Jesus Christ has come into your life, reuniting you with the Father in Heaven (John 14:6). Love. Forgiveness. Joy. These blessings and so much more have come upon you (Ephesians 1:3-14). Welcome to God’s family!<br/>" +
-    "<br/>" +
-    "Now, we all have our different stories, but there is only one story that unites us all – the story of God. We believe that everyone’s life-story matters because we know that God is always at work in and through our lives, whether or not we know or acknowledge it. That’s why we invite you to join us, as we lovingly share each other’s lives, experiences and stories, and as we take part in God’s glorious story of life – a story of hope, purpose and promise in our Lord Jesus Christ! <br/>"
+
+"Everyone enjoys a good story. Good thing every one of us has our own stories to tell—even our own “life’s story,” so to speak. But wouldn’t it be wonderful to know how each one’s individual story is connected with those of others’? We believe that, like a quilt that is sewn together by patches of different colors and designs, we are all attached to a broad canvas that unites the patches of our lives together to form a blanket of a beautiful and meaningful pattern. This beautiful “quilt” of connected stories is what we can call the <b>story of life</b>. So..." +
+"<br/>" +
+
+"<h3>What is the story of life?</h3>"+
+"Your whole life depends on how you answer this question. You live your life according to the story you believe you are a part of.<br/>" +
+"<ul>" +
+    "<li>" +
+        "If you believe the story of life is a story of chance, then you live believing that you are just a product of accidents, and that everything you do ultimately doesn’t really mean anything.<br/>" +
+    "</li>" +
+    "<li>" +
+        "If you believe the story of life is a story of struggle for power, then you will see everyone else as only either enemies to overpower or obstacles to overcome.<br/>" +
+    "</li>" +
+    "<li>" +
+        "If you believe the story of life is a story of enjoying everything in life while you can, then you spend life chasing all the pleasures you can get your hands on in this temporary world.<br/>" +
+    "</li>" +
+"</ul>" +
+
+
+"<h3>What is life according to God?</h3>" +
+"According to the Bible, this is God’s story....<br/>" +
+"<br/>" +
+
+"<b><i>God Created</i></b>" +
+"<br/>" +
+"Everything starts with God (Genesis 1:1). In fact, God is the center of all things. Everything moves and exists around Him and His good, pleasing and perfect will (Romans 11:36, 12:2b). Great. Beautiful. Infinitely glorious. This is God!" +
+"<br/><br/>" +
+"Then, this great, great God began to create. This is the beginning of the universe—the beginning of life, and the birth of humanity. God and man had a good relationship." +
+"<br/><br/>" +
+
+"<b><i>Man Sinned</i></b>" +
+"<br/>" +
+"However, humanity soon disobeyed their Creator. This is called sin . From then on, all of humanity became sinful. All became sinners (no exception!), separated from God (Romans 3:23) and condemned to experience His righteous wrath — eternal punishment in hell. This is eternal death (Romans 6:23a).<br/>" +
+"<br/>" +
+
+"<b><i>God Redeemed</i></b>" +
+"<br/>" +
+"But God, being rich in mercy, out of His great love (Ephesians 2:4), sent a Savior — Jesus Christ — who came to: " +
+"Live a perfect life (1 Peter 2:22), die a sacrificial death (1 Peter 2:24; Romans 5:8), and rise again to life (John 2:22), for the sake of those who will believe in him as Savior and Lord (John 3:16) — this is salvation. Those who believe have been forgiven forever (Ephesians 1:7), and are given the right to become blessed children of God (John 1:12), and have received life, life that continues beyond death… indeed, eternal life in God’s presence(John 5:24, 1 John 3:14), fully and forever (Psalm 16:11)!<br/>" +
+"<br/>" +
+
+"<h3>How, then, should life be lived?</h3>" +
+"Life, then, must be lived by faith in Jesus Christ (Romans 1:17). Faith or believing in Jesus means trusting in Him alone for your salvation (1 Timothy 2:5), acknowledging and turning away from your sins (Acts 3:19), making a decision to give your whole life to Him as your Lord (Romans 10:9), and delighting in and treasuring Him as the source of perfect joy (Matthew 13:44; Psalm 16:11). <br/>" +
+"<br/>" +
+"This salvation is only received not by our works or any merits coming from us, but only by God’s amazing grace, purely undeserved blessing, freely given to His people (Ephesians 2:8-9; Romans 6:23).<br/>" +
+"<br/>" +
+"Do you believe? If yes, then it means you have been born again into God’s Kingdom (John 3:3-6)! Newness of life has come (Romans 6:4; 2 Corinthians 5:17), because the Lord Jesus Christ has come into your life, reuniting you with the Father in Heaven (John 14:6). Love. Forgiveness. Joy. These blessings and so much more have come upon you (Ephesians 1:3-14). Welcome to God’s family!<br/>" +
+"<br/>" +
+"Now, we all have our different stories, but there is only one story that unites us all – the story of God. We know that our lives aren’t just pointless stories, like stories of chance, struggle for power or enjoyment of the temporary, because God has plotted for us a story that is infinitely more beautiful and satisfying. We believe that everyone’s life-story matters because we know that God is always at work in and through our lives, whether or not we know or acknowledge it. That’s why we invite you to join us as we lovingly share each other’s lives, experiences and stories, and as we take part in God’s glorious story of life – a story of hope, purpose and promise in our Lord Jesus Christ!"
